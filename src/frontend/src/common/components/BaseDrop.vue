125feda496e409c858bff28082e2401462049891
insulineru <insuline.ru@gmail.com>
2021-07-06 17:29:19 +0300

    feat(drag&drop): добавил hoc компоненты

---
+++ src/frontend/src/common/components/BaseDrop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div @drop.stop="onDrop" @dragover.prevent @dragenter.prevent>
+    <slot />
+  </div>
+</template>
+
+<script>
+import { DATA_TRANSFER_PAYLOAD } from "@/common/constants";
+
+export default {
+  name: "BaseDrop",
+
+  methods: {
+    onDrop({ dataTransfer }) {
+      if (!dataTransfer) {
+        return;
+      }
+      const payload = dataTransfer.getData(DATA_TRANSFER_PAYLOAD);
+      if (payload) {
+        const transferData = JSON.parse(
+          dataTransfer.getData(DATA_TRANSFER_PAYLOAD)
+        );
+        this.$emit("drop", transferData);
+      }
+    },
+  },
+};
+</script>
