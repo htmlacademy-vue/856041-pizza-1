e8eeb04a2cc3e339469735dea3a4af4a89a27002
insulineru <insuline.ru@gmail.com>
2021-07-07 22:37:29 +0300

    feat(builder): компонент ввода названия

---
+++ src/frontend/src/modules/builder/components/BuilderNameInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <base-input
+    v-model="name"
+    name="pizza_name"
+    placeholder="Введите название пиццы"
+  >
+    <span class="visually-hidden">Название пиццы</span>
+  </base-input>
+</template>
+
+<script>
+import BaseInput from "@/common/components/BaseInput.vue";
+
+export default {
+  name: "BuilderNameInput",
+
+  components: {
+    BaseInput,
+  },
+
+  model: {
+    prop: "value",
+    change: "change",
+  },
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    name: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        return this.$emit("change", val);
+      },
+    },
+  },
+};
+</script>
