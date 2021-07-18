158d2f28a28e11d7e639bb8940782bbd4607f5a2
insulineru <insuline.ru@gmail.com>
2021-07-18 14:54:00 +0300

    feat(cart): работа с данными в форме

--- src/frontend/src/modules/cart/components/CartForm.vue
+++ src/frontend/src/modules/cart/components/CartForm.vue
@@ -4,40 +4,40 @@
       <label class="cart-form__select">
         <span class="cart-form__label">Получение заказа:</span>
 
-        <select name="test" class="select">
-          <option value="1">Заберу сам</option>
-          <option value="2">Новый адрес</option>
-          <option value="3">Дом</option>
+        <select v-model="deliveryType" class="select">
+          <option value="pickup">Заберу сам</option>
+          <option value="delivery">Новый адрес</option>
+          <!-- <option value="3">Дом</option> -->
         </select>
       </label>
 
-      <label class="input input--big-label">
+      <base-input
+        class="input--big-label"
+        v-model="phone"
+        placeholder="+7 999-999-99-99"
+      >
         <span>Контактный телефон:</span>
-        <input type="text" name="tel" placeholder="+7 999-999-99-99" />
-      </label>
+      </base-input>
 
-      <div class="cart-form__address">
+      <div class="cart-form__address" v-if="deliveryType !== 'pickup'">
         <span class="cart-form__label">Новый адрес:</span>
 
         <div class="cart-form__input">
-          <label class="input">
+          <base-input v-model="street" required>
             <span>Улица*</span>
-            <input type="text" name="street" />
-          </label>
+          </base-input>
         </div>
 
         <div class="cart-form__input cart-form__input--small">
-          <label class="input">
+          <base-input v-model="house" required>
             <span>Дом*</span>
-            <input type="text" name="house" />
-          </label>
+          </base-input>
         </div>
 
         <div class="cart-form__input cart-form__input--small">
-          <label class="input">
+          <base-input v-model="flat">
             <span>Квартира</span>
-            <input type="text" name="apartment" />
-          </label>
+          </base-input>
         </div>
       </div>
     </div>
@@ -45,7 +45,68 @@
 </template>
 
 <script>
+import { SET_DELIVERY_PARAM } from "@/store/mutations-types";
+import { mapMutations } from "vuex";
+import BaseInput from "@/common/components/BaseInput.vue";
+
 export default {
   name: "CartForm",
+
+  components: {
+    BaseInput,
+  },
+
+  computed: {
+    deliveryType: {
+      get() {
+        return this.$store.state.Cart.delivery.type;
+      },
+      set(val) {
+        this.setDeliveryParam({ param: "type", value: val });
+      },
+    },
+
+    street: {
+      get() {
+        return this.$store.state.Cart.delivery.street;
+      },
+      set(val) {
+        this.setDeliveryParam({ param: "street", value: val });
+      },
+    },
+
+    house: {
+      get() {
+        return this.$store.state.Cart.delivery.house;
+      },
+      set(val) {
+        this.setDeliveryParam({ param: "house", value: val });
+      },
+    },
+
+    flat: {
+      get() {
+        return this.$store.state.Cart.delivery.flat;
+      },
+      set(val) {
+        this.setDeliveryParam({ param: "flat", value: val });
+      },
+    },
+
+    phone: {
+      get() {
+        return this.$store.state.Cart.delivery.phone;
+      },
+      set(val) {
+        this.setDeliveryParam({ param: "phone", value: val });
+      },
+    },
+  },
+
+  methods: {
+    ...mapMutations("Cart", {
+      setDeliveryParam: SET_DELIVERY_PARAM,
+    }),
+  },
 };
 </script>
