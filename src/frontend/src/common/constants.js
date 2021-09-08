5e5be540083538a0185ef22091affb475ef9d2f9
Insuline <insuline.ru@gmail.com>
2021-09-08 17:07:06 +0300

    refactor(base input): переписал на бинд атрибутов

--- src/frontend/src/common/constants.js
+++ src/frontend/src/common/constants.js
@@ -2,17 +2,3 @@ export const DATA_TRANSFER_PAYLOAD = "payload";
 export const MOVE = "move";
 
 export const MAX_INGREDIENTS_COUNT = 3;
-
-export const INPUT_TYPES = [
-  "button",
-  "checkbox",
-  "file",
-  "hidden",
-  "image",
-  "password",
-  "radio",
-  "reset",
-  "submit",
-  "text",
-  "email",
-];
