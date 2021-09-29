5600a861f45b6f5d9bf001a48c734123c1d346b9
Insuline <insuline.ru@gmail.com>
2021-09-29 19:02:17 +0300

    fix(profile): добавил загрузку адресов

--- src/frontend/src/views/Profile.vue
+++ src/frontend/src/views/Profile.vue
@@ -13,6 +13,7 @@
 <script>
 import ProfileInfo from "@/modules/profile/components/ProfileInfo.vue";
 import ProfileAddresses from "@/modules/profile/components/ProfileAddresses.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "ProfileIndex",
@@ -20,6 +21,14 @@ export default {
   components: {
     ProfileAddresses,
     ProfileInfo,
+  },
+
+  async mounted() {
+    await this.loadAddresses();
+  },
+
+  methods: {
+    ...mapActions("Auth", ["loadAddresses"]),
   },
 };
 </script>
